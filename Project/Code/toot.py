f71cae414d18be5925c19ebccb77f0b4e9c5e994
ottomattas <otto.mattas@gmail.com>
2021-02-22 09:19:27 +0100

    Initialise toot.py

---
+++ Project/Code/toot.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""This is the initial prototype for toot.ai"""
+
+__author__ = "Otto Mättas, Filippo Libardi, Daniele Di Grandi, Jim Ekanem"
+__copyright__ = "Copyright 2021, toot.ai"
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Otto Mättas"
+__email__ = "o.mattas@students.uu.nl"
+__status__ = "Testing"
